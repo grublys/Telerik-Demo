a94edf2fd8116db58921c4fec0b3c3d5acb18c78
Matthew Boston <matthew.boston@gmail.com>
2011-09-01 10:54:56 -0400

    seeding database with contacts

--- demo-app/db/seeds.rb
+++ demo-app/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+Contact.delete_all
+Contact.create(first_name: 'John', last_name: 'Smith',
+               email: 'john.smith@test.com', linkedin_profile: 'http://linkedin.com/in/john-smith',
+               lead_type: 'HOT')
+Contact.create(first_name: 'Jane', last_name: 'Doe',
+               email: 'jane.doe@example.com', linkedin_profile: 'http://linkedin.com/in/jane-doe',
+               lead_type: 'NEUTRAL')
+
+# TEMPLATE
+# Contact.create(first_name: '', last_name: '',
+#                email: '', linkedin_profile: '',
+#                lead_type: '')
