8678dbe887aa4938a364733b4f1ace2546214e80
Matthew Boston <matthew.boston@gmail.com>
2011-09-06 17:42:45 -0400

    making a bit more user friendly

--- demo-app/config/routes.rb
+++ demo-app/config/routes.rb
@@ -1,5 +1,5 @@
 DemoApp::Application.routes.draw do
-  root :to => 'welcome#index'
+  root :to => 'contacts#index'
   get 'logout' => 'sessions#destroy', :as => 'logout'
   get 'login' => 'sessions#new', :as => 'login'
   get 'help' => 'help#index', :as => 'help'
