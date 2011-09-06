1fdbb3250afe1038bb7dcd6517c2ae522752a290
Matthew Boston <matthew.boston@gmail.com>
2011-09-05 21:13:53 -0400

    move definition of helper_method

--- demo-app/app/controllers/application_controller.rb
+++ demo-app/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :require_login
+  helper_method :current_user
 
   private
 
@@ -13,5 +14,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
 end
