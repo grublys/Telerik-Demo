fb5986e70334cdedc0ad3582adec110cf933c669
Matthew Boston <matthew.boston@gmail.com>
2011-09-07 12:49:01 -0400

    lead type icon on contact view page

--- demo-app/app/helpers/contacts_helper.rb
+++ demo-app/app/helpers/contacts_helper.rb
@@ -1,2 +1,8 @@
 module ContactsHelper
+  def lead_type_icon(lead_type)
+    lead_type_icons = { 'HOT' => 'hot-icon.png',
+                        'COLD' => 'cold-icon.png',
+                        'NEUTRAL' => 'neutral-icon.png' }
+    lead_type_icons[lead_type]
+  end
 end
