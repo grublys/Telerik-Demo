5991b6fe32fd15b0beb06537320dc8f6dbb96cd5
Matthew Boston <matthew.boston@gmail.com>
2011-09-08 14:12:51 -0400

    new seeds for db. run `rake db:seed` to load new data

--- demo-app/db/seeds.rb
+++ demo-app/db/seeds.rb
@@ -2,11 +2,14 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 Contact.delete_all
-Contact.create(first_name: 'John', last_name: 'Smith',
-               email: 'john.smith@test.com', linkedin_profile: 'http://linkedin.com/in/john-smith',
+Contact.create(first_name: 'Jim', last_name: 'Holmes',
+               email: 'jim@iterativerose.com', linkedin_profile: 'http://www.linkedin.com/pub/jim-holmes/2/124/b02',
                lead_type: 'HOT')
-Contact.create(first_name: 'Jane', last_name: 'Doe',
-               email: 'jane.doe@example.com', linkedin_profile: 'http://linkedin.com/in/jane-doe',
+Contact.create(first_name: 'Todd', last_name: 'Kaufman',
+               email: 'toddkaufman@gmail.com', linkedin_profile: 'http://www.linkedin.com/pub/todd-kaufman/2/a58/1b1',
+               lead_type: 'HOT')
+Contact.create(first_name: 'Matthew', last_name: 'Boston',
+               email: 'matthew.boston@gmail.com', linkedin_profile: 'http://www.linkedin.com/in/mboston',
                lead_type: 'NEUTRAL')
 
 User.delete_all
