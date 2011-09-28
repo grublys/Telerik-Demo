ad9f5645558accf005b49ee9430a48005daa7488
Matthew Boston <matthew.boston@gmail.com>
2011-09-28 14:28:10 -0400

    adding html5 video support for firefox and safari

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,11 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+Rack::Mime::MIME_TYPES.merge!({
+  ".mp4" => "video/mp4",
+  ".m4v" => "video/mp4",
+  ".mov" => "video/quicktime",
+  ".ogg" => "video/ogg",
+  ".webm" => "video/ogg"
+})
