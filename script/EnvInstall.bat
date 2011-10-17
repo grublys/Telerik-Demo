edb0ea84eb035660438850221e8c855c9ba35de0
Todd Kaufman <toddkaufman@gmail.com>
2011-10-17 17:11:31 -0400

    Added devkit install for ruby

--- script/EnvInstall.bat
+++ script/EnvInstall.bat
@@ -17,22 +17,23 @@ path=%path%;%CD%\utils\bin;%CD%\utils\Git1.7.6\bin;%CD%\utils\Ruby1.9.2\bin
 
 
 @echo ----------------------------------------------------------------
-@ECHO STEP 2/5: Installing Git ...
-@echo ----------------------------------------------------------------
-IF NOT EXIST Git-1.7.6-preview20110708.exe wget http://msysgit.googlecode.com/files/Git-1.7.6-preview20110708.exe Git-1.7.6-preview20110708.exe
-Git-1.7.6-preview20110708.exe /silent /dir=%CD%\utils\Git1.7.6
-@echo+
-@echo+
-
-
-@echo ----------------------------------------------------------------
-@ECHO STEP 3/5: Installing Ruby ...
+@ECHO STEP 2/5: Installing Ruby ...
 @echo ----------------------------------------------------------------
 IF NOT EXIST rubyinstaller-1.9.2-p290.exe wget http://rubyforge.org/frs/download.php/75127/rubyinstaller-1.9.2-p290.exe rubyinstaller-1.9.2-p290.exe
 rubyinstaller-1.9.2-p290.exe /silent /dir="%CD%\utils\Ruby1.9.2"
 @echo+
 @echo+
 
+
+@echo ----------------------------------------------------------------
+@ECHO STEP 3/5: Installing DevKit ...
+@echo ----------------------------------------------------------------
+IF NOT EXIST DevKit-tdm-32-4.5.1-20101214-1400-sfx.exe wget --no-check-certificate https://github.com/downloads/oneclick/rubyinstaller/DevKit-tdm-32-4.5.1-20101214-1400-sfx.exe
+DevKit-tdm-32-4.5.1-20101214-1400-sfx.exe /silent /dir=%CD%\DevKit
+call ruby dk.rb init
+call ruby dk.rb install
+@echo+
+@echo+
 
 @echo ----------------------------------------------------------------
 @ECHO STEP 4/5: Setting up .gemrc ...
