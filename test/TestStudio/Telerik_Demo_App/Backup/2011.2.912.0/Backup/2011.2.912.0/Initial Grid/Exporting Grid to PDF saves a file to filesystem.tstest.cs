00415bbf1a52ab8596a222308f58b57244e0abf1
Jim Holmes <Jim.Holmes@Telerik.com>
2011-11-15 10:45:01 -0500

    Updating all tests

---
+++ test/TestStudio/Telerik_Demo_App/Backup/2011.2.912.0/Backup/2011.2.912.0/Initial Grid/Exporting Grid to PDF saves a file to filesystem.tstest.cs
@@ -0,0 +1,93 @@
+using Telerik.WebAii.Controls.Html;
+using Telerik.WebAii.Controls.Xaml;
+using Telerik.WebAii.Controls.Xaml.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using ArtOfTest.Common.UnitTesting;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Controls.HtmlControls.HtmlAsserts;
+using ArtOfTest.WebAii.Design;
+using ArtOfTest.WebAii.Design.Execution;
+using ArtOfTest.WebAii.ObjectModel;
+using ArtOfTest.WebAii.Silverlight;
+using ArtOfTest.WebAii.Silverlight.UI;
+
+namespace Telerik_Demo_App
+{
+
+    //
+    // You can add custom execution steps by simply
+    // adding a void function and decorating it with the [CodedStep] 
+    // attribute to the test method. 
+    // Those steps will automatically show up in the test steps on save.
+    //
+    // The BaseWebAiiTest exposes all key objects that you can use
+    // to access the current testcase context. [i.e. ActiveBrowser, Find ..etc]
+    //
+    // Data driven tests can use the Data[columnIndex] or Data["columnName"] 
+    // to access data for a specific data iteration.
+    //
+    // Example:
+    //
+    // [CodedStep("MyCustom Step Description")]
+    // public void MyCustomStep()
+    // {
+    //        // Custom code goes here
+    //      ActiveBrowser.NavigateTo("http://www.google.com");
+    //
+    //        // Or
+    //        ActiveBrowser.NavigateTo(Data["url"]);
+    // }
+    //
+        
+
+    public class Exporting_Grid_to_PDF_saves_a_file_to_filesystem : BaseWebAiiTest
+    {
+        #region [ Dynamic Pages Reference ]
+
+        private Pages _pages;
+
+        /// <summary>
+        /// Gets the Pages object that has references
+        /// to all the elements, frames or regions
+        /// in this project.
+        /// </summary>
+        public Pages Pages
+        {
+            get
+            {
+                if (_pages == null)
+                {
+                    _pages = new Pages(Manager.Current);
+                }
+                return _pages;
+            }
+        }
+
+        #endregion
+        
+        // Add your test methods here...
+    
+        [CodedStep(@"New Coded Step")]
+        public void Exporting_Grid_to_PDF_saves_a_file_to_filesystem_CodedStep()
+        {
+            Assert.IsTrue(System.IO.File.Exists(GetExtractedValue("downloadPath").ToString()));
+            
+        }
+    
+        [CodedStep(@"New Coded Step")]
+        public void Initialize()
+        {
+            string downloadPath = @"d:\temp\contacts.pdf";
+            SetExtractedValue("downloadPath", downloadPath );
+            
+            if (System.IO.File.Exists(downloadPath)) {
+                System.IO.File.Delete(downloadPath);
+            }
+        }
+    }
+}
