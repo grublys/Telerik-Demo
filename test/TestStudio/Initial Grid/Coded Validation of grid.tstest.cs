285afa4f624dda2a803fa7e8fcfca90b785a428f
Jim Holmes <Jim.Holmes@Telerik.com>
2012-01-28 22:14:42 -0500

    Changed connect to window step to accept data parameter for URL, changed to partial URL match. Much more robust, less brittle.

--- test/TestStudio/Initial Grid/Coded Validation of grid.tstest.cs
+++ test/TestStudio/Initial Grid/Coded Validation of grid.tstest.cs
@@ -87,6 +87,8 @@ namespace Telerik_Demo_App
             HtmlTableRow row = table.Find.TableRow(contactName);
 
             HtmlAnchor cell = row.Find.ByContent<HtmlAnchor>("View Contact");
+            string windowUrl = cell.HRef;
+            SetExtractedValue("contactWindowUrl", windowUrl);
 
             cell.Click();            
         }
